6d5a35194d74caab4256c3777580e7894516a07e
David Wiryawan <37268446+Sympathizee@users.noreply.github.com>
2023-08-17 16:14:29 +0700

    Add files via upload

---
+++ assets/Autos-32e0d8ec.js
@@ -0,0 +1,1 @@
+import{a8 as a,X as t,Y as s,Z as o,_ as c,av as _,aw as n,a1 as d}from"./index-4c0ff20f.js";const p={},r=e=>(_("data-v-c5f1bbb8"),e=e(),n(),e),l=r(()=>d("h1",null,"Page Automotive",-1));function u(e,f){return t(),s(c,{class:"ml-10 pl-10"},{default:o(()=>[l]),_:1})}const i=a(p,[["render",u],["__scopeId","data-v-c5f1bbb8"]]);export{i as default};
